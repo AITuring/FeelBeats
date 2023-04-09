5f3bd9c5d10a9e5ea3edf711c48c8c52751705c9
AITuring <shspku365@gmail.com>
2023-04-09 22:24:15 +0800

    only drawer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { useState, useRef, useEffect } from 'react';
-import _, { isArray } from 'lodash';
+import _ from 'lodash';
 import './App.css';
 import Header from './components/Header';
 import Player from './components/Player';
@@ -14,44 +14,90 @@ function App() {
   const [words, setWords] = useState('');
   // 默认输入模式，输入模式不可播放音乐
   const [inputMode, setInputMode] = useState(true);
+  const inputRef = useRef(null);
   // 生成的音乐列表
   const [musicList, setMusicList] = useState([]);
+  // 当前播放的音乐
+  const [currentMusic, setCurrentMusic] = useState(mp3);
   // 历史数据
-  const [history, setHistory] = useState([
-    {
-      time: '2023.04.08 21:33',
-      word: '虽然很难，但我想尽力赢下这场比赛。',
-      emotion: '得意',
-      background: ColorList.find(item => item.name.includes('得意')).color
-    }
-  ]);
+  const [history, setHistory] = useState([]);
   // 展示侧边栏
   const [showDrawer, setShowDrawer] = useState(false);
   // 播放音乐
   const [isPlaying, setIsPlaying] = useState(false);
+  // 图片name
+  const [imgName, setImgName] = useState('');
+  // 图片文件
+  const [imgSrc, setImgSrc] = useState('');
 
   const debouncedSetWords = _.debounce((value) => setWords(value), 500);
-
+  // 获取情感，音乐列表
   useEffect(() => {
     if (words.length > 0) {
       fetch(`/api/emotion/text-emotion?message=${words}`).then(response => response.json())
         .then(data => {
-          setEmotion(ColorList.find(item => {
-            if (item.name.includes(data.emotion)) {
-              return item;
-            } else {
-              // 无感
-              return ColorList[1];
-            }
-          }));
+          const res = ColorList.find(item => item.name.includes(data.emotion));
+          console.log(res)
+          if (res) {
+            setEmotion(res);
+          } else {
+            // 无感
+            setEmotion(ColorList[1]);
+          }
           setInputMode(false);
-          setMusicList(isArray(data.music) ? data.music : []);
-          // 返回音乐就播放
           setIsPlaying(true);
+          setMusicList(data.musicname.split(','));
+
         })
         .catch(error => console.error(error));
     }
   }, [words])
+
+  // 给输入框赋值
+  useEffect(() => {
+    if (inputMode && inputRef.current && words.length > 0) {
+      inputRef.current.value = words;
+    }
+  }, [words, inputMode, inputRef]);
+
+  // 获取音乐url
+  useEffect(() => {
+    if (musicList.length > 0) {
+      setCurrentMusic(`http://192.168.10.96:64641/music?fileName=${musicList[0]}`);
+    }
+  }, [musicList]);
+
+  // 获取图片url
+  useEffect(() => {
+    if (imgName) {
+      setImgSrc(`http://192.168.10.96:64641/img?imgPath=${imgName}`);
+    }
+  }, [imgName])
+
+  // 获取历史列表
+  useEffect(() => {
+    fetch('/cards').then(response => response.json()).then(data => {
+      setHistory(data);
+      console.log(data)
+    })
+  }, [])
+
+  // 保存卡片
+  const createCard = () => {
+    const formData = new FormData();
+    formData.append('emotionTag', emotion.name[0]);
+    formData.append('imgPath', imgName);
+    formData.append('musicName', musicList[0]);
+    formData.append('text', words);
+
+    fetch('/card', {
+      method: 'POST',
+      body: formData
+    }).then(response => response.json()).then(data => {
+      setShowDrawer(true);
+    })
+
+  }
 
   return (
     <div
@@ -61,9 +107,12 @@ function App() {
       }}
     >
       <Header
-        emotion={emotion}
         musicList={musicList}
         setIsPlaying={setIsPlaying}
+        isPlaying={isPlaying}
+        inputMode={inputMode}
+        setCurrentMusic={setCurrentMusic}
+        setImgName={setImgName}
       />
       {/* 输入模式 */}
       {
@@ -71,7 +120,7 @@ function App() {
           <textarea
             placeholder='写下你的故事或此刻的情绪'
             className='input'
-            value={words}
+            ref={inputRef}
             onChange={(e) => debouncedSetWords(e.target.value)}
             onKeyDown={(e) => {
               if (e.key === 'Enter') {
@@ -85,8 +134,9 @@ function App() {
       {/* 非输入模式展示 */}
       {
         !inputMode && (
-          <>
+          <div className='showZone'>
             <div className='words'>{words}</div>
+            <img src={imgSrc} alt="" className='imgShow' />
             <div className='buttons'>
               <div
                 className='button'
@@ -95,21 +145,23 @@ function App() {
                   // input框回显
                   setInputMode(true);
                   setIsPlaying(false);
+                  // 返回默认情感
+                  setEmotion(ColorList[0]);
                 }}
               >
                 编辑
               </div>
-              <div className='saveBtn' onClick={() => setShowDrawer(false)}>
+              <div className='saveBtn' onClick={() => createCard()}>
                 保存
               </div>
             </div>
-          </>
+          </div>
         )
       }
       <Player
         isPlaying={isPlaying}
         setIsPlaying={setIsPlaying}
-        src={mp3}
+        src={currentMusic}
         disabled={inputMode}
         coverColor={emotion.color}
       />
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,22 +1,70 @@
+import { useEffect, useState } from 'react';
 import logo from '../../imgs/logo.svg';
 import uploadImg from '../../imgs/uploadImg.png';
 import musicIcon from '../../imgs/musicIcon.svg';
 import musicActive from '../../imgs/musicActive.svg';
 import refresh from '../../imgs/refresh.svg';
+import { Upload, message } from 'antd';
 import './style.css'; // 导入组件样式
 
+
 const Header = (props) => {
+    const [currentIcon, setCurrentIcon] = useState(musicIcon);
+
+    const showIcon = () => {
+        // 正在播放，可以刷新
+        if (props.isPlaying) {
+            setCurrentIcon(refresh);
+        }
+        if (props.musicList.length > 0) {
+            setCurrentIcon(musicActive);
+        }
+        if (props.inputMode) {
+            setCurrentIcon(musicIcon);
+        }
+    };
+
+    const uploadProps = {
+        name: 'file',
+        action: 'http://192.168.10.96:64641/fileUpload',
+        showUploadList: false,
+        onChange(info) {
+            if (info.file.status !== 'uploading') {
+                console.log(info.file, info.fileList);
+            }
+            if (info.file.status === 'done') {
+                console.log(info.file.response)
+                props.setImgName(info.file.response);
+                message.success(`${info.file.name} file uploaded successfully`);
+            } else if (info.file.status === 'error') {
+                message.error(`${info.file.name} file upload failed.`);
+            }
+        },
+    };
+
+    useEffect(() => {
+        showIcon();
+    }, [props]);
+
     return (
         <div className="header">
-            <img src={logo} alt="logo" className='logo'/>
+            <img src={logo} alt="logo" className='logo' />
             <div className="bar">
-                <img src={uploadImg} alt="uploadImg" className='icon' />
+                <Upload {...uploadProps}>
+                    <img src={uploadImg} alt="uploadImg" className='icon' />
+                </Upload>
                 <div className="musicIcon">
                     <img
-                        src={props.musicList.length > 0 ? musicActive : musicIcon}
+                        src={currentIcon}
                         alt="musicIcon"
                         className={props.musicList.length > 0 ? 'iconEnter' : 'iconNotEnter'}
                         onClick={() => {
+                            // 已经播放，点击切换音乐
+                            if (props.isPlaying) {
+                                // TODO
+                                return
+                            }
+
                             if (props.musicList.length > 0) {
                                 props.setIsPlaying(!props.isPlaying);
                             }
